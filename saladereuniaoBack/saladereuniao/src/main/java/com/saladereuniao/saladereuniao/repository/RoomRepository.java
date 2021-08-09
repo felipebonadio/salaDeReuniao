3a349f671ef059c3b8424d7d3f1328e7c6c10772
Felipe Chagas Bonadio <felipe.bonadio@etec.sp.gov.br>
2021-08-09 16:33:20 -0300

    entrega do projeto

---
+++ saladereuniaoBack/saladereuniao/src/main/java/com/saladereuniao/saladereuniao/repository/RoomRepository.java
@@ -0,0 +1,12 @@
+package com.saladereuniao.saladereuniao.repository;
+
+import com.saladereuniao.saladereuniao.model.Room;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository  extends JpaRepository<Room, Long>{
+
+}
